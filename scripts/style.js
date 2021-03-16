10094319de70381a73cf839abeaab11d3b83928a
Anna Nguyen <axn3@cornell.edu>
2021-03-16 16:51:35 -0400

    Clicking outside of all filter dropdowns removes filters

--- scripts/style.js
+++ scripts/style.js
@@ -1,4 +1,14 @@
 $(document).ready(function () {
-
+  window.onclick = function (event) {
+    if (!event.target.matches('.filter-dropdown-button')) {
+      var dropdowns = document.getElementsByClassName("filter-dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show-dropdown')) {
+          openDropdown.classList.remove('show-dropdown');
+        }
+      }
+    }
+  }
 
 })
